2b58a7b36668236d2f50795df236e564585fc8bc
inamasu <inamasu0204@gmail.com>
2025-09-11 19:55:41 +0900

    ファイル削除

--- AutoTool/Core/Descriptors/ICommandRegistry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoTool.Core.Descriptors;
-
-
-public interface ICommandRegistry
-{
-    IEnumerable<ICommandDescriptor> All { get; }
-    ICommandDescriptor Get(string type);
-    bool TryGet(string type, out ICommandDescriptor? descriptor);
-}
